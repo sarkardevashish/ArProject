ec7579b0e1d0e0189c2c046988c57e9d6a62cb34
devashish <devashish.sarkar@evontech.com>
2022-08-29 17:27:15 +0530

    tried adding button

--- main.js
+++ main.js
@@ -140,20 +140,7 @@ const createScene = async () => {
     return scene;
 };
 
-// GUI
-var advancedTexture =
-    BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click Me");
-button1.width = "150px";
-button1.height = "40px";
-button1.color = "white";
-button1.cornerRadius = 20;
-button1.background = "green";
-button1.onPointerUpObservable.add(function () {
-    alert("you did it!");
-});
-advancedTexture.addControl(button1);
 
 // const sceneToRender = createScene();
 
